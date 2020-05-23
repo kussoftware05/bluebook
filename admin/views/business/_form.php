9979a222ef1d519ccd5ffa429c13a42fa3bdfb9a
deepaksingh <kussoftware05@gmail.com>
2020-05-23 14:55:18 +0530

    code updated

--- admin/views/business/_form.php
+++ admin/views/business/_form.php
@@ -5,6 +5,7 @@ use yii\widgets\ActiveForm;
 use yii\helpers\ArrayHelper;
 use dosamigos\ckeditor\CKEditor;
 use admin\models\User;
+use admin\models\BusinessDirectory;
 /* @var $this yii\web\View */
 /* @var $model admin\models\Blog */
 /* @var $form yii\widgets\ActiveForm */
@@ -21,19 +22,22 @@ use admin\models\User;
                 <?= $form->field($model, 'business_name')->textInput(['maxlength' => true]) ?>
                 
                 <?php $user = User::find()->all();$listData = ArrayHelper::map($user,'id','first_name');?>
-                
+                <?php $countries = BusinessDirectory::getCountries();?>
+				<?php $states = BusinessDirectory::getStates();?>
 				<?= $form->field($model, 'description')->widget(CKEditor::className(), ['options' => ['rows' => 6], 'preset' => 'basic']) ?>
 				<?=Html::label('Banner Image')?>
 				<?php if($model->bannerimg!=''){?>
 				<div><img src="<?= Yii::$app->request->baseUrl.'/images/bannerImage/'.$model->bannerimg ?>" height="150" width="200"/></div>
 				<?php } ?>
-				<?=Html::label('Banner Image')?>
 				<?= $form->field($model, 'bannerimg')->fileInput(['accept' => 'image/*'])->label(false); ?>
 				<?= $form->field($model, 'duration')->textInput(['maxlength' => true]) ?>
 				<?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
 				<?= $form->field($model, 'contactno')->textInput(['maxlength' => true])->label('Contact') ?>
 				<?= $form->field($model, 'userId')->dropDownList($listData, ['prompt' => 'Select User'])->label('User') ?>
+				
 				<?=Html::label('Location')?>
+				<?= $form->field($model, 'countryId')->dropDownList($countries, ['prompt' => 'Select Country'])->label('Country') ?>
+				<?= $form->field($model, 'stateId')->dropDownList($states, ['prompt' => 'Select State'])->label('State') ?>
 				<?= $form->field($model, 'city')->textInput(['maxlength' => true]) ?>
 				<?= $form->field($model, 'otherinfo')->textInput(['maxlength' => true]) ?>
 				<?= $form->field($model, 'textlink')->textInput(['maxlength' => true]) ?>
